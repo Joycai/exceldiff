caf5f86b0282a3872a09b89d9a40d71ec6c99ed5
joycai <joycai@joycai.cn>
2021-06-24 17:51:11 +0800

    first commit

---
+++ src/main/kotlin/joycai/exceldiff/ExceldiffApplication.kt
@@ -0,0 +1,11 @@
+package joycai.exceldiff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExceldiffApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExceldiffApplication>(*args)
+}
---
+++ src/test/kotlin/joycai/exceldiff/ExceldiffApplicationTests.kt
@@ -0,0 +1,13 @@
+package joycai.exceldiff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExceldiffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
